04ac6825cda59f3d8d11acf59cb46ab560f61a82
devlopersabbir <devlopersabbir@gmail.com>
2023-08-31 16:18:26 +0600

    now able to do everything

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 
-const App = () => {
-  return <div>App</div>;
+const App: React.FC = () => {
+  return (
+    <div className="container bg-slate-100">
+      <h1 className="text-blue-600">Hello</h1>
+      <p>hello new</p>
+    </div>
+  );
 };
 
 export default App;
